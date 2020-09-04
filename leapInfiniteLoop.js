0cf934e2a2b3f8d4137f97dcd965904bbd85d9af
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-09-04 15:25:39 -0400

    Update leapInfiniteLoop.js
    step 54

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,11 +5,13 @@ var y = window.innerHeight / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
-  clear();
+  console.log(frame.hands);
 
-  var rando1 = Math.floor(Math.random() * 2) - 1;
-  var rando2 = Math.floor(Math.random() * 2) - 1;
-
-  circle(x + rando1, y + rando2, 50);
+  // clear();
+  //
+  // var rando1 = Math.floor(Math.random() * 2) - 1;
+  // var rando2 = Math.floor(Math.random() * 2) - 1;
+  //
+  // circle(x + rando1, y + rando2, 50);
 }
 );
