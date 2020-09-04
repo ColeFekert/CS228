b3c197fdd99cc4a36b796e2ef2932e1ac9e8a825
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-09-04 15:08:46 -0400

    Update leapInfiniteLoop.js
    Circle get erased each loop and i has been removed.
    Step 50.

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,17 +1,14 @@
 var controllerOptions = {};
 
-var i = 0;
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
-  console.log(i);
+  clear();
 
   var rando = Math.floor(Math.random() * 2) - 1;
 
   circle(x + rando, y, 50);
-
-  i++;
 }
 );
