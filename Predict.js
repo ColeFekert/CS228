acdc051f32d2b2dd5472d2e4fca012a46d429034
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-09-27 15:55:34 -0400

    Update Predict.js
    Step 7

--- Predict.js
+++ Predict.js
@@ -1,4 +1,16 @@
 
 function draw() {
   clear();
+
+  Train();
+
+  Test();
+}
+
+function Train() {
+  console.log("I am being trained.");
+}
+
+function Test() {
+  console.log("I am being tested.");
 }
