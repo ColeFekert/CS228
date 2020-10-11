06ec2a1e9072c2c4c31329c4d8f7f96564cf2a83
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-10-11 13:50:08 -0400

    step 4

--- PredictGestures.js
+++ PredictGestures.js
@@ -23,6 +23,8 @@ var count = 0;
 
 var controllerOptions;
 
+var moreThanOneHand;
+
 // var predictedClassLabels = nj.zeros(numSamples);
 
 
@@ -38,6 +40,8 @@ Leap.loop(controllerOptions, function(frame) {
   if (!trainingCompleted) {
     Train();
   }
+
+  HandleFrame(frame);
 
   Test();
 });
@@ -99,4 +103,155 @@ function GotResults(err, result) {
   if (testingSampleIndex >= test.shape[3]) {
     testingSampleIndex = 0;
   }
+}
+
+function HandleFrame(frame) {
+  var interactionBox = frame.interactionBox;
+
+  if (frame.hands.length == 1){
+    moreThanOneHand = false;
+
+    var hand = frame.hands[0];
+
+    HandleHand(hand, moreThanOneHand, interactionBox);
+  } else if (frame.hands.length > 1) {
+    moreThanOneHand = true;
+
+    var hand = frame.hands[0];
+
+    HandleHand(hand, moreThanOneHand, interactionBox);
+  } else {
+    moreThanOneHand = false;
+  }
+}
+
+function HandleHand(hand, moreThanOneHand, interactionBox) {
+  var fingers = hand.fingers;
+
+  for (var i = 3; i >= 0; i -= 1) {     // For each bone
+    for (var j = 4; j >= 0; j -= 1) {   // For each finger
+      // HandleFinger(fingers[i]);
+      // console.log("finger: " + j);
+      // console.log("bone: " + i + "\n");
+      HandleBone(fingers[j].bones[i], fingers[j].bones[i].type, j, moreThanOneHand, interactionBox);
+    }
+  }
+}
+
+function HandleBone(bone, boneType, fingerIndex, moreThanOneHand, interactionBox) {
+  var normalizedPrevJoint = interactionBox.normalizePoint(bone.prevJoint, true);
+  var normalizedNextJoint = interactionBox.normalizePoint(bone.nextJoint, true);
+
+  // xt = bone.nextJoint[0];
+  // yt = bone.nextJoint[1];
+  // zt = bone.nextJoint[2];
+  //
+  // xb = bone.prevJoint[0];
+  // yb = bone.prevJoint[1];
+  // zb = bone.prevJoint[2];
+
+  xt = normalizedNextJoint[0];
+  yt = normalizedNextJoint[1];
+  zt = normalizedNextJoint[2];
+
+  xb = normalizedPrevJoint[0];
+  yb = normalizedPrevJoint[1];
+  zb = normalizedPrevJoint[2];
+
+
+  // console.log("Normalized Prev Joint: " + normalizedPrevJoint);
+  // console.log("Normalized Next Joint: " + normalizedNextJoint);
+
+  // framesOfData.set(fingerIndex, boneType, 0, currentSample, xb);
+  // framesOfData.set(fingerIndex, boneType, 1, currentSample, yb);
+  // framesOfData.set(fingerIndex, boneType, 2, currentSample, zb);
+  // framesOfData.set(fingerIndex, boneType, 3, currentSample, xt);
+  // framesOfData.set(fingerIndex, boneType, 4, currentSample, yt);
+  // framesOfData.set(fingerIndex, boneType, 5, currentSample, zt);
+
+
+  var canvasPrevX = window.innerWidth * normalizedPrevJoint[0];
+  var canvasPrevY = window.innerHeight * (1 - normalizedPrevJoint[1]);
+
+  var canvasNextX = window.innerWidth * normalizedNextJoint[0];
+  var canvasNextY = window.innerHeight * (1 - normalizedNextJoint[1]);
+
+  // console.log(framesOfData);
+
+  // console.log("X Range: " + window.innerWidth + " | Y Range: " + window.innerHeight);
+  // console.log("CanvasPrevX: " + canvasPrevX + " | CanvasPrevY: " + canvasPrevY);
+  // console.log("CanvasNextX: " + canvasNextX + " | CanvasNextY: " + canvasNextY);
+
+  // [xt, yt] = TransformCoordinates(xt, yt);
+  // [xb, yb] = TransformCoordinates(xb, yb);
+
+
+  // framesOfData.set(boneType, fingerIndex, 0, xb);
+  // framesOfData.set(boneType, fingerIndex, 1, yb);
+  // framesOfData.set(boneType, fingerIndex, 2, zb);
+  // framesOfData.set(boneType, fingerIndex, 3, xt);
+  // framesOfData.set(boneType, fingerIndex, 4, xt);
+  // framesOfData.set(boneType, fingerIndex, 5, xt);
+
+  // Draws both hands as gray
+  if (boneType == 0) {
+    strokeWeight(25);
+    // stroke(20);
+    stroke(207);
+  } else if (boneType == 1) {
+    strokeWeight(20);
+    // stroke(60);
+    stroke(158);
+  } else if (boneType == 2) {
+    strokeWeight(15);
+    // stroke(80);
+    stroke(115);
+  } else {
+    strokeWeight(10);
+    // stroke(100);
+    stroke(67);
+  }
+
+  // Changes the color of the hands from green to red when more than one are present
+  // if (moreThanOneHand) {
+  //   if (boneType == 0) {
+  //     strokeWeight(8);
+  //     // stroke(20);
+  //     stroke(255,0,0);
+  //   } else if (boneType == 1) {
+  //     strokeWeight(6);
+  //     // stroke(60);
+  //     stroke(207,0,0);
+  //   } else if (boneType == 2) {
+  //     strokeWeight(4);
+  //     // stroke(80);
+  //     stroke(158,0,0);
+  //   } else {
+  //     strokeWeight(2);
+  //     // stroke(100);
+  //     stroke(115,0,0);
+  //   }
+  // } else {
+  //   if (boneType == 0) {
+  //     strokeWeight(8);
+  //     // stroke(20);
+  //     stroke(0,255,0);
+  //   } else if (boneType == 1) {
+  //     strokeWeight(6);
+  //     // stroke(60);
+  //     stroke(0,207,0);
+  //   } else if (boneType == 2) {
+  //     strokeWeight(4);
+  //     // stroke(80);
+  //     stroke(0,158,0);
+  //   } else {
+  //     strokeWeight(2);
+  //     // stroke(100);
+  //     stroke(0,115,0);
+  //   }
+  // }
+
+  line(canvasNextX, canvasNextY, canvasPrevX, canvasPrevY, zt, zb);
+
+  // line(xt, window.innerHeight - yt, xb, window.innerHeight - yb, zt, zb);
 }
