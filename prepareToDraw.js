629a1091a6105a094ba236df3d22fd2048c00e35
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-10-26 11:25:26 -0400

    Added more images

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,8 +1,13 @@
-var img;
+var imgOver, imgLeft, imgRight, imgUp, imgDown, imgAway, imgTowards;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  img = loadImage('https://i.imgur.com/aNd6xdH.jpeg');
-  // img = loadImage('Hand-over-the-device.jpg');
+  imgOver = loadImage('https://i.imgur.com/aNd6xdH.jpeg');
+  imgLeft = loadImage('https://i.imgur.com/KTrMkQZ.jpg');
+  imgRight = loadImage('https://i.imgur.com/b1wVlkv.jpg');
+  imgUp = loadImage('https://i.imgur.com/e3QNAOq.jpg');
+  imgDown = loadImage('https://i.imgur.com/HOOGynt.jpg');
+  imgAway = loadImage('https://i.imgur.com/n6IS297.jpg');
+  imgTowards = loadImage('https://i.imgur.com/PcRJbBD.jpg');
 }
