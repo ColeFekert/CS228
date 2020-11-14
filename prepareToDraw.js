28ae26153a22914eece3e5ea919a53cf24ad17cb
Cole Fekert <49728995+ColeFekert@users.noreply.github.com>
2020-11-14 17:59:11 -0500

    Added Digit Images

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,5 +1,5 @@
 var imgOver, imgLeft, imgRight, imgUp, imgDown, imgAway, imgTowards;
-var imgZero, imgOne;
+var imgZero, imgOne, imgTwo, imgThree, imgFour, imgFive, imgSix, imgSeven, imgEight, imgNine;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
@@ -14,4 +14,12 @@ function setup() {
 
   imgZero = loadImage('https://i.imgur.com/A3LJvO4.png');
   imgOne = loadImage('https://i.imgur.com/R6MJwFw.png');
+  imgTwo = loadImage('https://i.imgur.com/F7NSOjl.png');
+  imgThree = loadImage('https://i.imgur.com/EFUI1tj.png');
+  imgFour = loadImage('https://i.imgur.com/nnQmsl0.png');
+  imgFive = loadImage('https://i.imgur.com/tscuSdH.png');
+  imgSix = loadImage('https://i.imgur.com/YrMaOUO.png');
+  imgSeven = loadImage('https://i.imgur.com/g9gwoUR.png');
+  imgEight = loadImage('https://i.imgur.com/7P0aXhr.png');
+  imgNine = loadImage('https://i.imgur.com/mv7dDcH.png');
 }
